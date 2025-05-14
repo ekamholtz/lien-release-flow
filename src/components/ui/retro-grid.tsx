3ed2fd08080ed99ae10302c83ad82a380a871d31
ekamholtz <ekamholtz@cnstrctnetwork.com>
2025-05-14 12:07:10 -0700

    feat: edit RetroGrid component with animated gradient background using company colors

--- src/components/ui/retro-grid.tsx
+++ src/components/ui/retro-grid.tsx
@@ -24,17 +24,17 @@ export function RetroGrid({
 
             "[background-repeat:repeat] [background-size:60px_60px] [height:300vh] [inset:0%_0px] [margin-left:-50%] [transform-origin:100%_0_0] [width:600vw]",
 
-            // Light Styles
-            "[background-image:linear-gradient(to_right,rgba(0,0,0,0.3)_1px,transparent_0),linear-gradient(to_bottom,rgba(0,0,0,0.3)_1px,transparent_0)]",
+            // Light Styles - Using company colors
+            "[background-image:linear-gradient(to_right,rgba(23,35,115,0.3)_1px,transparent_0),linear-gradient(to_bottom,rgba(255,92,28,0.2)_1px,transparent_0)]",
 
-            // Dark styles
-            "dark:[background-image:linear-gradient(to_right,rgba(255,255,255,0.2)_1px,transparent_0),linear-gradient(to_bottom,rgba(255,255,255,0.2)_1px,transparent_0)]",
+            // Dark styles - Using company colors
+            "dark:[background-image:linear-gradient(to_right,rgba(42,59,161,0.4)_1px,transparent_0),linear-gradient(to_bottom,rgba(255,92,28,0.3)_1px,transparent_0)]",
           )}
         />
       </div>
 
       {/* Background Gradient */}
-      <div className="absolute inset-0 bg-gradient-to-t from-white to-transparent to-90% dark:from-black" />
+      <div className="absolute inset-0 bg-gradient-to-t from-white via-transparent to-transparent to-90% dark:from-cnstrct-navy/20" />
     </div>
   );
 }
